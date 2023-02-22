35d2fa073bbb8396519da6964b36b5f44320ea69
Yudha Styawan <39240883+yudhastyawan@users.noreply.github.com>
2023-02-22 14:40:46 +0700

    Update checkif3comps.py

--- precondition/checkif3comps.py
+++ precondition/checkif3comps.py
@@ -1,11 +1,14 @@
 import obspy as ob
 
-st = ob.read("test_vel_non_lempeng.mseed")
+file_input = "T1.mseed"
+file_output = "T1_3comps.mseed"
+
+st = ob.read(file_input)
 if len(st) > 3:
     print(f"data memiliki {len(st)} komponen > 3 komponen")
     print(f"menggabungkan data . . . ")
-    st.merge(fill_value='interpolate')
-    st.write("test_vel_non_lempeng_merge_2.mseed", format="MSEED")
+    st.merge(method=1, interpolation_samples=-1, fill_value='interpolate')
+    st.write(file_output, format="MSEED")
     print(f"selesai")
 else:
     print("data memiliki 3 komponen")
